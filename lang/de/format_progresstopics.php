f821105f9c8a91700106079746b78e5d002de004
rtschu <robin.tschudi@gmail.com>
2020-10-02 17:53:38 +0200

    fixed nameoverflow, fixed wrong sectionnames

--- lang/de/format_progresstopics.php
+++ lang/de/format_progresstopics.php
@@ -1,4 +1,18 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 $string['addsections'] = 'Abschnitte hinzufügen';
 $string['currentsection'] = 'Dieser Abschnitt';
@@ -9,6 +23,7 @@ $string['newsectionname'] = 'Neuer name für Abschnitt {$a}';
 $string['sectionname'] = 'Progresstopis';
 $string['pluginname'] = 'Progresstopics format';
 $string['section0name'] = 'Allgemein';
+$string['sectionplaceholdername'] = $string['sectionname'] . ' {$a}';
 $string['page-course-view-topics'] = 'Kursstartseite im Progresstopic Format';
 $string['page-course-view-topics-x'] = 'Eine Kursseite im Progresstopic Format';
 $string['hidefromothers'] = 'Abschnitt verbergen';
